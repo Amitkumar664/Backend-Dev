e5bf6dd5ac403ab4865faaf2ce68d33b41b5a689
Amit Kumar <amitkumar4102004b@gmail.com>
2026-01-09 15:06:00 +0530

    added some files

--- FS/server.js
+++ FS/server.js
@@ -84,9 +84,11 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
-    // console.log(req.method); 
-    res.end('Hello from the server');
+    console.log(req.url);
+    console.log(req.method); 
+    res.writeHead(200,{'Content-Type':'plain/text'});
+    res.write("<h1>Hello from the server</h1>");
+    res.end();
 });
 server.listen(3000,()=>{
     console.log('server is listening on port 3000',3000);
