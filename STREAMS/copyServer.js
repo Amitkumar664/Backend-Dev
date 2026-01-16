533164b2d3f7f5e389de89c4ae1d09f7919d28c2
Amit Kumar <amitkumar4102004b@gmail.com>
2026-01-16 09:19:55 +0530

    added cop server

---
+++ STREAMS/copyServer.js
@@ -0,0 +1,18 @@
+const fs=require('fs');
+
+const readStream=fs.createReadStream('./log.txt')
+
+const writeStream=fs.createWriteStream('./copyLog.txt');
+
+// readStream.on('data',(chunk)=>{
+//     let data=chunk.toString();
+//     writeStream.write(data);
+// });
+
+// readStream.on('end',()=>{
+//     writeStream.end();
+//     console.log('Copy completed');
+// });
+
+//pipeline
+readStream.pipe(writeStream);
