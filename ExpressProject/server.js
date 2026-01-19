54a9eb5d703391c948b08f6dbc13c7bdffbf509c
Amit Kumar <amitkumar4102004b@gmail.com>
2026-01-19 15:11:15 +0530

    added expreez file

--- ExpressProject/server.js
+++ ExpressProject/server.js
@@ -1,41 +1,48 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+// ✅ IMPORT CORRECTLY
+const { userData } = require('./data');
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-app.get('/user',(req,res)=>{
-    res.send('User endpoint');  
-})
-app.get('/home',(req,res)=>{
-    res.send('Home endpoint');
-})
-app.get('/about',(req,res)=>{
-    res.send('About endpoint');
-})
-app.get('/userdetail',(req,res)=>{
-    let user={
-        name:"Amit Kumar",
-        age:30,
-        email:" sdjbgjksg@sddgfnfk"}
-     res.status(200).json(user);
-})
-app.get('/homedetail',(req,res)=>{
-    let user={
-        name:"vasudhaera",
-        add:" mathira"}
-     res.status(200).json(user);
-})
-app.get('/aboutdetail',(req,res)=>{
-    let user={
-        name:"amit",
-        age:20,
-        email:" amitkumar4102004b@gmail.com"}
-     res.status(200).json(user);
-})
+
+app.get('/user', (req, res) => {
+  res.send('User endpoint');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Home endpoint');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About endpoint');
+});
+
+app.get('/userdetail', (req, res) => {
+  res.status(200).json({
+    name: "Amit Kumar",
+    age: 30,
+    email: "sdjbgjksg@sddgfnfk"
+  });
+});
 
 
+app.get('/userData', (req, res) => {
+  res.status(200).json(userData);
+});
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+// const adults = userData.filter(user => user.age > 25);
+// console.log(adults);
+app.get("/userAge",(req,res)=>{
+    let userGreaterThan25=userData.filter((user)=>user.age>25);
+    res.status(200).json(userGreaterThan25);
+    console.log(userGreaterThan25);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
