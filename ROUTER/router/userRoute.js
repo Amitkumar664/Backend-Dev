84b44efe4999df7ea9d8042e53d0618c12761f02
Amit Kumar <amitkumar4102004b@gmail.com>
2026-02-02 10:06:42 +0530

    added more route

--- ROUTER/router/userRoute.js
+++ ROUTER/router/userRoute.js
@@ -1,5 +1,12 @@
 import express from 'express';
 const router=express.Router();
+
+let logging=(req,res,next)=>{
+    console.log("this is middleware")
+    console.log(req.url)
+    next()
+};
+router.use(logging)
 router.get("/",(req,res)=>{
     res.send("server is running")
 });
--- ROUTER/server.js
+++ ROUTER/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoute from "./router/userRoute.js"
 import registrationRoute from "./router/registrationRoute.js"
+import dashboardRoute from "./router/dashboardRoute.js"
 const port=3000;
 const app=express();
 
@@ -14,7 +15,8 @@ const app=express();
 
 //middleware
 app.use("/api",userRoute)
-app.use("/api",registrationRoute)
+app.use("/reg",registrationRoute)
+app.use("/dh",dashboardRoute)
 app.listen(port,()=>{
     console.log("server is running");
 })
