516cdf486b986eee197fd073918404fc49bb4c3a
Amit Kumar <amitkumar4102004b@gmail.com>
2026-02-03 14:44:56 +0530

    added ststicfiles

--- STATICFILES/server.js
+++ STATICFILES/server.js
@@ -1,6 +1,13 @@
 import express from 'express'
 const app=express();
 const port=3000;
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const _filename=fileURLToPath(import.meta.url)
+const _dirname=path.dirname(_filename);
+
+const filepath=path.join(_dirname,"public")
 //middleware
 // app.use(express.static('public')); //it will get image from public as /img1.png   always use static if not express will not understand
 app.use("/static",express.static('public'));   ///static/img1.png
