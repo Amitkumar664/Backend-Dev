f4f9b31f221973648562f3aa52774d5b91578519
Amit Kumar <amitkumar4102004b@gmail.com>
2026-02-06 14:03:17 +0530

    added EJS

--- EJS/server.js
+++ EJS/server.js
@@ -10,12 +10,17 @@ app.get("/",(req,res)=>{
 app.get("/me",(req,res)=>{
     res.send("its me")
 })
-
-app.get("/user",(req,res)=>{
-    let userData={
+ let userData=[{
+        id:1,
         name:"amit",
         age:"23"
-    }
+    },{  id:2,
+        name:"Sumit",
+        age:"23"
+
+    }]
+app.get("/user",(req,res)=>{
+   
     res.render("user",{userData})
 })
 
@@ -24,6 +29,26 @@ app.get("/list",(req,res)=>{
     let arr=[]
 
     res.render("list",{arr})
+})
+app.use(express.urlencoded({extended:true}));
+app.post("/api/user",(req,res)=>{
+    const{name,age}=req.body;
+    let newUserData={
+        id:userData.length,
+        name,
+        age
+    }
+    userData.push(newUserData);
+    res.redirect('/user')
+})
+app.get("/user/user/:id/",(req,res)=>{
+    const userid=req.params.id;
+    const useridx=userData.findIndex((ele)=>ele.id==userid);
+    if (userid==-1){
+        return res.send("user not found") 
+    }
+    userData.splicce(useridx,1);
+    res.redirect("/user")
 })
 app.listen(3000,(req,res)=>{
     console.log("server is running")
